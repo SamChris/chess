2335b2b1ad2774d706fae477fbd8465a4117bac4
App Academy Student <student@appacademy.io>
2013-10-29 11:48:30 -0400

    Wrote Diagonal Moves Functions

---
+++ board.rb
@@ -0,0 +1,13 @@
+class Board
+  def initialize
+    @board = Array.new(8) {Array.new(8)}
+  end
+
+  def checked?(color)
+    #returns whether a player is checked
+  end
+
+  def move(start, end)
+  end
+
+end
--- chess.rb
+++ chess.rb
@@ -1,4 +1,13 @@
 class Piece
+  DIAG_DELTAS = [ [1,1],[-1,-1],[1,-1],[-1, 1]]
+
+
+  def initialize(board, pos)
+    @board = board
+    @pos = pos
+    @board[pos[0]][pos[1]]= self
+  end
+
 
   def moves
     places = []
@@ -7,18 +16,56 @@ class Piece
   def move
   end
 
-end
+  def move_into_check?(pos)
+  end
+
+  def get_diagonal_moves
+    diag_arr = []
+    DIAG_DELTAS.each do |delta|
+      diag_arr += get_moves(self.pos, delta)
+    end
+
+    diag_arr
+  end
+
+  def get_moves(pos, x_delta, y_delta)
+
+    move_arr = []
+    new_pos = [pos + x_delta, pos+y_delta]
+    return [] unless new_pos[0].between(0,7) && new_pos[1].between(0,7)
+
+    if @board[new_pos[0]][new_pos[1]].nil?
+      move_arr += new_pos
+      return move_arr + get_moves(new_pos, x_delta, y_delta)
+    end
+
+    return []
+  end
 
 class SlidingPiece < Piece
-  @board # has the board reference
+
 
   def moves(dir)
-
+    if dir == :diagonal
+      diag_moves = super.get_diag_moves
+      return diag_moves
+    elsif dir == :straight
+      straight = super.get_straight_moves
+      return straight
+    else
+      all = super.get_diag_moves
+      all += super.get_straight_moves
+      return all
+    end
   end
 
 end
 
 class Bishop < SlidingPiece
+
+  def initialize(pos)
+    super(pos)
+  end
 
 
   def move_dirs
